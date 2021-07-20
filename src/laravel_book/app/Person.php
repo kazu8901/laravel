22272ae4b6da47e7dbff69fd11b600a986fdfa47
kazu8901 <ka_8901@docomo.ne.jp>
2021-07-20 19:27:05 +0900

    スコープ・検索

--- src/laravel_book/app/Person.php
+++ src/laravel_book/app/Person.php
@@ -3,6 +3,9 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
+use App\Scopes\ScopePerson;
+
 
 class Person extends Model
 {
@@ -10,4 +13,23 @@ class Person extends Model
     public function getData() {
         return $this->id . ':' . $this->name . '(' . $this->age . ')';
     }
+
+    public function scopeNameEqual($query, $str) {
+        return $query->where('name', $str);
+    }
+
+    public function scopeAgeGreaterThan($query, $min) {
+        return $query->where('age', '>=', $min);
+    }
+
+    public function scopeAgeLessThan($query, $max) {
+        return $query->where('age', '<=', $max);
+    }
+
+    protected static function boot() {
+        parent::boot();
+    
+        static::addGlobalScope(new ScopePerson);
+    }
+    
 }
