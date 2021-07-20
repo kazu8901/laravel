c4e664a643f7a3d11ef4bb41837e361ed113709d
kazu8901 <ka_8901@docomo.ne.jp>
2021-07-20 16:36:03 +0900

    テス

--- src/laravel_book/resources/views/person/find.blade.php
+++ src/laravel_book/resources/views/person/find.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.helloapp')
 
-@section('title', 'find')
+@section('title', 'Person.find')
 
 @section('menubar')
  @parent
@@ -8,7 +8,19 @@
 @endsection
 
 @section('content')
-  
+  <form action="/person/find" method="post">
+    {{ csrf_field() }}
+    <input type="text" name="input" value="{{$input}}">
+    <input type="submit" value="検索">
+  </form>
+  @if (isset($item))
+    <table>
+      <tr><th>Data</th></tr>
+      <tr>
+        <td>{{$item->getData()}}</td>
+      </tr>
+    </table>
+  @endif
 @endsection
 
 @section('footer')
