498ee667b7274dcee50e6183f6c2ca93bb95135b
kazu8901 <ka_8901@docomo.ne.jp>
2021-07-20 16:05:36 +0900

    eloquent基本

---
+++ src/laravel_book/resources/views/person/find.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.helloapp')
+
+@section('title', 'find')
+
+@section('menubar')
+ @parent
+ 検索ページ
+@endsection
+
+@section('content')
+  
+@endsection
+
+@section('footer')
+manij
+@endsection
